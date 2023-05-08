5065be857a279458d6959274a5525f8c2defa3b7
Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>
2023-05-08 19:07:47 +0300

    Add changes to the title
    Also add new variables
    
    Signed-off-by: Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>

--- SimpleFileManager/DetailViewController.swift
+++ SimpleFileManager/DetailViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     var selectedImage: String?
+    var countPictures = 0
+    var numberOfPicture = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = selectedImage
+        title = "Picture \(numberOfPicture) of \(countPictures)"
         navigationItem.largeTitleDisplayMode = .never
         
         if let pictureToLoad = selectedImage {
@@ -30,15 +32,5 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

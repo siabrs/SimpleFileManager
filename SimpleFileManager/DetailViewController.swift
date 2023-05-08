cc716319841ad141e3bf4f19d970a53defb04dd6
Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>
2023-05-08 16:36:14 +0300

    Add title and property hidesBarsOnTap
    Signed-off-by: Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>

--- SimpleFileManager/DetailViewController.swift
+++ SimpleFileManager/DetailViewController.swift
@@ -14,11 +14,22 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
         if let pictureToLoad = selectedImage {
             imageView.image = UIImage(named: pictureToLoad)
         }
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 
     /*
     // MARK: - Navigation
